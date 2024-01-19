eb1eea8093982f86a44d8390cdba1ea4b10432e3
parthiprr <parthiprineesh100@gmail.com>
2024-01-19 17:28:28 +0530

    added jasmine

---
+++ Desktop/shopping/tests-jasmine/moneyTest.js
@@ -0,0 +1,14 @@
+import{formatCurrency} from'../scripts/utils/money.js';
+
+describe('test suite format currency',()=>
+{
+  it('convert cents into dollars ',()=>{
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+  it('work with 0 ',()=>{
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+  it('rounds up t0 the nearest cent ',()=>{
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+});
